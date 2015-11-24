c8763aac319e28573029385eda37434f56f00fd6
ryanlennon <ryanlennon07@gmail.com>
2015-11-24 13:45:01 -0600

    updated controller page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,23 +1,62 @@
 class ProductsController < ApplicationController
   def index
+    @product = Product.all
+
+    sort_attribute = params[:sort_attribute]
+    sort_order = params[:sort_order]
+    if sort_attribute && sort_order
+      @product = Product.order(sort_attribute => sort_order)
+    end
+
+    if params[:discount]
+      @product = Product.where("price < ?", 2)
   end
 
   def new
   end
 
   def create
+        @product = Product.create(
+      name: params[:name],
+      price: params[:price],
+      image: params[:image],
+      description: params[:description]
+      user_id: current_user.id
+      )
+    flash[:success] = "You have successfully added a product!"
+    redirect_to '/products'
   end
 
   def show
+    if params[:id] =="random"
+      products = Product.all
+      @product = product.sample
+    else
+    @product = Product.find_by(id: params[:id])
+    end
   end
 
   def edit
+    @product = Product.find_by(id: params[:id])
   end
 
   def update
+    @product = Product.find_by(id: params[:id])
+    product.update(
+      name: params[:name],
+      price: params[:price],
+      image: params[:image],
+      description: params[:description],
+      )
+    flash[:success] = "Information succesfully updated!"
+    redirect_to "/products/#{@product.id}"
   end
 
   def destroy
+    product = Product.find_by(id: params[:id])
+    product.destroy
+    flash[:success] = "Product successfully deleted!"
+    redirect_to '/products'
   end
-  
+
 end
