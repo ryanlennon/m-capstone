b7afd07dd633a21da2eabe5df82cc3e55cdba6ec
ryanlennon <ryanlennon07@gmail.com>
2015-11-24 18:05:46 -0600

    added mySQL2 from storefront app

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.create!([
+  {name: "Xbox 360", description: "Good condition, includes 3 rechargeable wireless controllers", price: "200", image: "xbox360.jpg"},
+  {name: "Xbox 360 games", description: "Assorted games for the Xbox 360", price: "20", image: "games.jpg"},
+  {name: "Turtle beach X510", description: "High qualityhead set for the Xbox 360", price: "25", image: "headset.jpg"},
+  {name: "Leather couch", description: "Black, three seated couch with parts of the leather flaking off", price: "25", image: "blackcouch.jpg"}
+])
