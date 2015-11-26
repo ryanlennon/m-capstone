1ae3e9cef6c2cce3333921cbe82541d732580e8f
ryanlennon <ryanlennon07@gmail.com>
2015-11-25 18:56:52 -0600

    added orders controller

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,9 @@
+class OrdersController < ApplicationController
+
+  def create
+  end
+
+  def show 
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ Rails.application.routes.draw do
   patch "products/:id" => 'products#update'
   delete "products/:id" => 'products#destroy'
 
+  get "/orders/new" => 'orders#new'
+  post "/orders/" => 'orders#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
